4b290b37df8828a4ce5eb67225bbc985ebb5ff3f
Kavitha1958 <suraganikavitha56@gmail.com>
2026-09-23 10:33:58 +0530

    Added Python lab programs

---
+++ LAB-1.py
@@ -0,0 +1,7 @@
+def display(strl,int_x,float_y):
+    print("The string is",strl)
+    print("The int_x is",int_x)
+    print("The float_y is",float_y)
+
+display("Hi",89,12.5)
+display(strl = "BCA", int_x = 9, float_y = 18.9)
